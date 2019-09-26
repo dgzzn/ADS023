3b9f28146a1537dff51ed4074eb48ca57147437b
Dgzin <xdgzin@hotmail.com>
2019-09-26 17:12:16 -0300

    Atividade 09,10

--- Atividade08/src/app/home/home.page.ts
+++ Atividade08/src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { ToastController } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'app-home',
@@ -8,17 +10,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor(toastCtrl: ToastController) {}
+  constructor(toastCtrl: ToastController, private alertCtrl: AlertController) {
+    
+  }
 
   statusBluetooth: Boolean = false;
 
   ligaBluetooth(status: Boolean){
     this.statusBluetooth = status;
-    exibaToast();
+    this.exibeToast();
+
+    if (this.statusBluetooth = false){
+      this.exibeAlert();
+    }
+
   }
 
-  async exibaToast(){
+  async exibeToast(){
     const msg = 'Bluetooth ligado!';
-    // Como eu faço essa merda aqui 2-j
+    const toast = await ToastController.create({
+      message: msg,
+      duration: 2000
+    })
+    toast.present()
+  }
+
+ async exibeAlert(){
+  const alert = await this.alertCtrl.create({
+    header: 'Confirmação',
+    message: 'Deseja ligar o Bluetooth do aparelho?',
+    buttons: [{
+        text: 'Cancel',
+        handler: () => { this.ligaBluetooth(false) }
+      }, {
+        text: 'Okay',
+        handler: () => { this.statusBluetooth = false }
+      }]
+    });
+    alert.present()
   }
 }
