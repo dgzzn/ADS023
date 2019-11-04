d0c0a408a4b8e3fe97ad3dd04b3ba17d3ea2cbc0
Dgzin <xdgzin@hotmail.com>
2019-11-04 13:35:23 -0300

    Atividade 11

---
+++ app/src/main/java/br/iesb/ads023/atividade11/MainActivity.kt
@@ -0,0 +1,38 @@
+package br.iesb.ads023.atividade11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate disparado.")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart disparado.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume disparado.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause disparado.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop disparado.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy disparado.")
+    }
+}
