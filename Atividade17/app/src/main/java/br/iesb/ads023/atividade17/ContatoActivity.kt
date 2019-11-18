90b3de3521fea688c221836f84190b8278ad13a7
Dgzin <xdgzin@hotmail.com>
2019-11-18 14:45:01 -0300

    Atividade17

---
+++ Atividade17/app/src/main/java/br/iesb/ads023/atividade17/ContatoActivity.kt
@@ -0,0 +1,14 @@
+package br.iesb.ads023.atividade17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContatoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contato)
+        setTitle("Novo contato")
+    }
+
+}
