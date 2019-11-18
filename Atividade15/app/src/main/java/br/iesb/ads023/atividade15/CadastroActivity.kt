720ce4ef7fd283201905c9ac1543e62c2f70879f
Dgzin <xdgzin@hotmail.com>
2019-11-18 14:07:58 -0300

    Atividade15

---
+++ Atividade15/app/src/main/java/br/iesb/ads023/atividade15/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.ads023.atividade15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
